f9f5aa1d10d2d9c5b3a1f0b62b918db3cb7cfad9
@gabcar10 <gabcarlo@free.fr>
2025-09-26 18:36:59 +0200

    Ch2, L4

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def get_book_text(file_path):
     with open(file_path) as f: # f is a file object
     #read the file
         file_contents = f.read()
-
     return file_contents
 
+def word_count(full_text):
+    #text is split & counted; works OK!
+    word_count = len(full_text.split())
+    return word_count
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    num_words = word_count(get_book_text("./books/frankenstein.txt"))
+    print(f"Found {num_words} total words")
 
 main()
