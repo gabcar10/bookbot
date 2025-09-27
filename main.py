cc9fe30efe9beee185b9d366a0422d8a0fb3278b
@gabcar10 <gabcarlo@free.fr>
2025-09-27 23:00:13 +0200

    Ch2, L5 refactoring

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_count
+
 #return content of the book as a string
 def get_book_text(file_path):
     #open the file    
@@ -5,11 +7,6 @@ def get_book_text(file_path):
     #read the file
         file_contents = f.read()
     return file_contents
-
-def word_count(full_text):
-    #text is split & counted; works OK!
-    word_count = len(full_text.split())
-    return word_count
 
 def main():
     num_words = word_count(get_book_text("./books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(full_text):
+    #text is split & counted; works OK!
+    word_count = len(full_text.split())
+    return word_count
