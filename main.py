7c7c62d169332bae17435f96362e5ab426272d9b
@gabcar10 <gabcarlo@free.fr>
2025-10-01 23:02:25 +0200

    Last version

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import word_count
+from stats import char_count
+from stats import sort_by_count
+import sys
 
 #return content of the book as a string
 def get_book_text(file_path):
@@ -9,7 +12,39 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    num_words = word_count(get_book_text("./books/frankenstein.txt"))
+    if len(sys.argv)==1:
+        print("Usage: python3 main.py <path_to_book> (books/Title)")
+        sys.exit(1)
+
+    print("============ BOOKBOT ============")
+    #book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
+    print(f"Analyzing book found at {book_path}")
+    #word counter
+    num_words = word_count(get_book_text(book_path))
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
+    #character counter (dictionary of occurrence)
+    dict_char = {}
+    unsorted_dict = {}
+    dict_char = char_count(get_book_text(book_path))
+    for char in dict_char:
+        count = dict_char[char]
+        unsorted_dict.update({char: count})
+        #print(f"character: {char}, occurrences: {count}; ")        #neater presentation
+        #print(f"'{char}': {count}")                                 #simpler, as required for the exercise
+    sorted_list = sort_by_count(unsorted_dict)
+    #print(sorted_list)
+    
+    print("----------- Character Count ----------")
+    for dico in sorted_list:
+        #discard non alphanumerical characters        
+        if dico["char"].isalpha():
+            print(f"{dico['char']}: {dico['num']}")
+
 main()
+
+'''NICE TO DO: How would I go about ordering (ASC or DESC) the result?
+as it stands, the characters are ordered on the base of which appears first in the document.
+I'd have to compare (loop on) the dictionary against a pre-ordered list and complete a new ordered dictionary.'''
--- stats.py
+++ stats.py
@@ -1,4 +1,43 @@
 def word_count(full_text):
-    #text is split & counted; works OK!
+    #text is split & counted
     word_count = len(full_text.split())
     return word_count
+
+def char_count(inp_string):
+    char_count = {}
+    for current_char in inp_string: 
+        #print(current_char.lower())     #debug OK
+        if current_char.lower() in char_count: #IF found (TRUE)
+            char_count[current_char.lower()] += 1   #increment current char
+            #print(f"add {current_char.lower()}; count {char_count[current_char.lower()]}")
+        else:                                       #create new record for the current character
+            char_count[current_char.lower()] = 1
+            #print(f"new char: {current_char.lower()}")
+    return char_count
+
+#-------------------------------------------------------------------------------------
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(items): 
+    return items['num']     
+
+def sort_by_count(dict):        #(dict{key, value})
+    #OK: Each dictionary should have two key-value pairs: one for the character itself and one for that character's count 
+    #(e.g. {"char": "b", "num": 4868}).
+    #print(f"nice! {dict['char']}, {dict['num']}")
+    current_dict = {}     #{"char": x, "num": 12} 
+    # .sort() method works on a list! So I need a list of dictionaries (see below).
+    list_dict = []       #[{"char": x, "num": 12}, {"char": y, "num": 40}]
+    for char in dict:
+        #discard non alphanumerical characters
+        #if char.isalpha():
+        current_dict["char"] = char
+        current_dict["num"] = dict[char]
+        list_dict.append({"char": char, "num": dict[char]})     #doesn't work WHEN list_dict.append(current_dict)
+            #print(current_dict)    #OK, 31 lines w/ good values
+            #print(list_dict)    #KO, if appending current_dict; OK when writing the dict (not the variable)
+    #print(list_dict)    #KO, if appending current_dict; OK when writing the dict (not the variable)
+    list_dict.sort(reverse=True, key=sort_on)
+    #print(list_dict)   #OK
+    return list_dict
